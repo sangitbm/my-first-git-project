e4d4ed9b58aad8765c6b4bafa85df2adde367e1f
Sangit BM <sangitbmktm@gmail.com>
2025-12-09 18:29:57 -0600

    This is my assignment.

---
+++ profile_sentence.py
@@ -0,0 +1,6 @@
+first_name = "Rahul"
+country = "USA"
+favorite_language = "Python"
+years_learning = 1
+
+print(first_name, "is from", country, "and is learning", favorite_language, "for", years_learning, "year(s).")
