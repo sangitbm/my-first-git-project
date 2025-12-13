03df64d5f2213756c95eb024e6857894e4688dcb
Sangit BM <sangitbmktm@gmail.com>
2025-12-12 18:19:30 -0600

    This is my first mini project

---
+++ mini_project1.py
@@ -0,0 +1,10 @@
+name = input("Enter your name:")
+birth_year = int(input("Enter your birth year:"))
+your_age= 2025-birth_year
+fav_language = input("Enter your favorite programming language:")
+
+print("Hello",name,"!")
+print("You are approximately",your_age,"years old.")
+print("Your favorite programming language is",fav_language,".")
+print("Nice to meet you.")
+
