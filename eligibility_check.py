8739199dc635f933b1a0479b6b3ef56ecfd305cc
Sangit BM <sangitbmktm@gmail.com>
2025-12-12 18:35:19 -0600

    Day 3 if else eligibility check

---
+++ eligibility_check.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+
+if age >= 21:
+    print("Hello",name,"you are eligible.")
+
+else:
+    print("Sorry",name,"you are not eligible.")
+
