beb436b73d0832214e51308d30932d60ff5da1bd
Sangit BM <sangitbmktm@gmail.com>
2025-12-12 20:10:01 -0600

    This is my day 4 practice

---
+++ day4_elif.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age:"))
+
+if age < 13:
+    print("You are child.")
+
+elif age < 20:
+    print("You are teenager.")
+
+elif age < 60:
+    print("You are an adult.")
+
+else:
+    print("You are senior.")
