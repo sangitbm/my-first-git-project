82dcfc7d356b8f565ccbb1c272a67a5f639b41f6
Sangit BM <sangitbmktm@gmail.com>
2025-12-12 20:37:28 -0600

    This is my boolean practice.

---
+++ day5_boolean.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age:"))
+has_id = input("Do you have an ID? (yes/no)") =="yes"
+
+if age >=18 and has_id :
+    print("You can enter the club.")
+
+else:
+    print("You cannot enter.")
